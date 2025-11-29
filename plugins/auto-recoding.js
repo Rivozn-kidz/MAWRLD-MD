6925e366d3dc307046f32b2d66daad1943cc8b96
Rivozn kidz <smtechofcmodz@outlook.com>
2025-11-29 21:20:29 +0300

    Update auto-recoding.js

--- plugins/auto-recoding.js
+++ plugins/auto-recoding.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const config = require('../config)
-const {marwld , commands} = require('../marwld')
+const config = require('../config')
+const {lite , commands} = require('../marwld')
 
 
 //auto recording
-marwld({
+lite({
   on: "body"
 },    
 async (conn, mek, m, { from, body, isOwner }) => {       
