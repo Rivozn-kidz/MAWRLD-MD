13128b98ead090baf125900e9cd757fb6c093a1b
Rivozn kidz <smtechofcmodz@outlook.com>
2025-11-29 21:21:59 +0300

    Update auto-typing.js

--- plugins/auto-typing.js
+++ plugins/auto-typing.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const config = require('../config')
-const {marwld , commands} = require('../marwld')
+const {lite , commands} = require('../marwld')
 
 
 // Composing (Auto Typing)
-marwld({
+lite({
     on: "body"
 },    
 async (conn, mek, m, { from, body, isOwner }) => {
