71a240823ff4d7a0c571a72dc5db3e2f9b571018
Rivozn kidz <smtechofcmodz@outlook.com>
2025-12-12 01:59:48 +0300

    Update get-owner.js

--- plugins/get-owner.js
+++ plugins/get-owner.js
@@ -3,7 +3,7 @@ const config = require('../config');
 
 lite({
     pattern: "owner",
-    react: "✅", 
+    react: "🔮", 
     desc: "Get owner number",
     category: "main",
     filename: __filename
@@ -13,18 +13,37 @@ async (conn, mek, m, { from }) => {
         const ownerNumber = config.OWNER_NUMBER;
         const ownerName = config.OWNER_NAME;
 
-        const vcard = 'BEGIN:VCARD\n' +
-                      'VERSION:3.0\n' +
-                      `FN:${ownerName}\n` +  
-                      `TEL;type=CELL;type=VOICE;waid=${ownerNumber.replace('+', '')}:${ownerNumber}\n` + 
-                      'END:VCARD';
+        const caption = 
+`*Hello 👋, I am Ridz Coder.*
 
-        // Only send contact card
+_I laugh at everyone who laughs at me._
+
+*I am the last thief*, but don't chase after me  
+because I will change myself.
+
+Ask them all and they will tell you:
+
+• If you stand *behind me*, I protect you.  
+• If you stand *beside me*, I respect you.  
+• But if you stand *against me*, I show *no mercy*.
+
+👤 *${ownerName}*  
+📞 *${ownerNumber}*`;
+
+        const vcard =
+            'BEGIN:VCARD\n' +
+            'VERSION:3.0\n' +
+            `FN:${ownerName}\n` +
+            `TEL;type=CELL;type=VOICE;waid=${ownerNumber.replace('+', '')}:${ownerNumber}\n` +
+            'END:VCARD';
+
+        // Send contact card with styled caption
         await conn.sendMessage(from, {
             contacts: {
                 displayName: ownerName,
                 contacts: [{ vcard }]
-            }
+            },
+            caption: caption
         });
 
     } catch (error) {
