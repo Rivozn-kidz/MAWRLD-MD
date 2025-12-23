0928ddaa0a878ff6a54937922a7d9b1239005b98
Rivozn kidz <smtechofcmodz@outlook.com>
2025-12-23 13:38:21 +0300

    Update tool-tts.js

--- plugins/tool-tts.js
+++ plugins/tool-tts.js
@@ -7,7 +7,7 @@ lite({
     pattern: "tts",
     desc: "download songs",
     category: "download",
-    react: "👧",
+    react: "🏔️",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
